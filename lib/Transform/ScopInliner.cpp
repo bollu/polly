a8911dc7b30bd01ba7c0d19174fcc468758ac7ba
Siddharth Bhat <siddu.druid@gmail.com>
2017-08-17 17:50:01 +0200

    [NFC] fix style comments, make ScopInliner check that -polly-allow-full-func is enabled

--- lib/Transform/ScopInliner.cpp
+++ lib/Transform/ScopInliner.cpp
@@ -14,55 +14,39 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "polly/CodeGen/CodeGeneration.h"
-#include "polly/CodeGen/IslAst.h"
-#include "polly/CodeGen/IslNodeBuilder.h"
-#include "polly/CodeGen/PPCGCodeGeneration.h"
-#include "polly/CodeGen/Utils.h"
-#include "polly/DependenceInfo.h"
-#include "polly/LinkAllPasses.h"
-#include "polly/Options.h"
-#include "polly/ScopDetection.h"
-#include "polly/ScopInfo.h"
-#include "polly/Support/SCEVValidator.h"
-#include "llvm/Analysis/AliasAnalysis.h"
-#include "llvm/Analysis/BasicAliasAnalysis.h"
-#include "llvm/Analysis/CaptureTracking.h"
-#include "llvm/Analysis/GlobalsModRef.h"
-#include "llvm/Analysis/ScalarEvolutionAliasAnalysis.h"
-#include "llvm/Analysis/TargetLibraryInfo.h"
-#include "llvm/Analysis/TargetTransformInfo.h"
-#include "llvm/Analysis/CallGraphSCCPass.h"
-#include "llvm/IR/LegacyPassManager.h"
-#include "llvm/IR/Verifier.h"
-#include "llvm/IRReader/IRReader.h"
-#include "llvm/Linker/Linker.h"
-#include "llvm/Support/TargetRegistry.h"
-#include "llvm/Support/TargetSelect.h"
-#include "llvm/Target/TargetMachine.h"
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
-#include "llvm/Transforms/Utils/BasicBlockUtils.h"
-#include "llvm/Transforms/Utils/ModuleUtils.h"
-#include "llvm/Passes/PassBuilder.h"
-#include "llvm/Transforms/IPO/AlwaysInliner.h"
 
 #define DEBUG_TYPE "polly-scop-inliner"
+
+#include "llvm/IR/LLVMContext.h"
+#include "polly/ScopDetection.h"
+#include "llvm/Analysis/CallGraphSCCPass.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "polly/RegisterPasses.h"
+#include "polly/LinkAllPasses.h"
+#include "llvm/Transforms/IPO/AlwaysInliner.h"
+
+using namespace polly;
+extern bool polly::PollyAllowFullFunction;
+
 namespace {
 class ScopInliner : public CallGraphSCCPass {
 public:
   static char ID;
 
+  ScopInliner() : CallGraphSCCPass(ID) { }
 
-  ScopInliner() : CallGraphSCCPass(ID) {
+  bool doInitialization(CallGraph &CG) override {
+      if (!polly::PollyAllowFullFunction) {
+          report_fatal_error("Aborting from ScopInliner because it only makes sense "
+                             "to run this with -polly-allow-full-function. The heurtistic "
+                             "for ScopInliner checks that the full function is a Scop, which only happens if "
+                             "polly-allow-full-function is enabled. If not, the entry block is not included in the Scop");
+
+      }
+      return true;
   }
-  /// runOnSCC - This method should be implemented by the subclass to perform
-  /// whatever action is necessary for the specified SCC.  Note that
-  /// non-recursive (or only self-recursive) functions will have an SCC size of
-  /// 1, where recursive portions of the call graph will have SCC size > 1.
-  ///
-  /// SCC passes that add or delete functions to the SCC are required to update
-  /// the SCC list, otherwise stale pointers may be dereferenced.
-  ///
+
    bool runOnSCC(CallGraphSCC &SCC) override {
        PassBuilder PB;
        FunctionAnalysisManager FAM;
@@ -75,7 +59,12 @@ public:
       assert(SCC.size() == 1 && "found empty SCC");
       Function *F = (*SCC.begin())->getFunction();
 
-      if (!F || F->isDeclaration()) return false;
+      // If the function is a nullptr, or the function is a declaration.
+      if (!F) return false;
+      if (F->isDeclaration()) {
+          DEBUG(dbgs() << "Skipping " << F->getName() << "because it is a declaration.\n");
+      }
+
       RegionInfo &RI = FAM.getResult<RegionInfoAnalysis>(*F);
       ScopDetection &SD = FAM.getResult<ScopAnalysis>(*F);
 
@@ -93,9 +82,7 @@ public:
           MPM.run(*M, MAM);
       }
 
-
-
-      errs() << "-" << F->getName() << " | " << HasScopAsTopLevelRegion << "\n";
+      DEBUG(dbgs() << F->getName() << " has scop as top level region:  " << HasScopAsTopLevelRegion << "\n");
       return false;
 
   };
